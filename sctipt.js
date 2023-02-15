72475a66ad3bea5309e1bba26519267a2d60bac3
SahilZore-2003 <106907644+SahilZore-2003@users.noreply.github.com>
2023-02-15 17:46:00 +0530

    adding footer

--- sctipt.js
+++ sctipt.js
@@ -48,4 +48,28 @@ function events() {
   });
 }
 
+// footer code 
+let hide = document.querySelectorAll('.hide');
+let message = document.querySelector('#message');
+let times = document.querySelector('#times');
+let show = ()=>{
+    hide.forEach((showing)=>{
+        showing.style.display = "block";
+        message.style.display = "none"
+    })
+}
+
+times.addEventListener('click',()=>{
+    hide.forEach((showing)=>{
+        showing.style.display = "none";
+        message.style.display = "block"
+    })
+})
+
+let youtube = ()=>{
+    alert("sorry we dont have youtube channel!!")
+}
+
+// footer code ends 
+
 events();
